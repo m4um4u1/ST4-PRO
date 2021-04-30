1554319b63707f18551ca919f9b6f3d5f0d724bd
madsc16 <Capp4d0cius2019>
2021-04-30 13:49:37 +0200

    Iteration 1 CommandHandler

---
+++ commandhandler/src/main/java/dk/sdu/mmmi/semproject/commandhandler/Main.java
@@ -0,0 +1,6 @@
+package dk.sdu.mmmi.semproject.commandhandler;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ commandhandler/src/test/java/dk/sdu/mmmi/semproject/commandhandler/CommandhandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.semproject.commandhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandhandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
