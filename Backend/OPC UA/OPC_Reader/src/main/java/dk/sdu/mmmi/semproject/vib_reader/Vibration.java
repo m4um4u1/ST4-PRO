cf0b71d92fcda2b55f7268eeaebc1069737a5a92
Saiuan Ahmad <saiuan@msn.comn>
2021-05-25 12:55:55 +0200

    I have refactored the modules into their respective layers. We now have a Backend layer, for the webserver and OPC java backend, A data folder, for the sensors, and a Frontend folder, for the visual representation

--- Backend/OPC UA/OPC_Reader/src/main/java/dk/sdu/mmmi/semproject/vib_reader/Vibration.java
+++ Backend/OPC UA/OPC_Reader/src/main/java/dk/sdu/mmmi/semproject/vib_reader/Vibration.java
@@ -3,8 +3,6 @@ package dk.sdu.mmmi.semproject.vib_reader;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.topic.ITopic;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 public class Vibration {
     HazelcastInstance hz = HazelcastClient.newHazelcastClient();
